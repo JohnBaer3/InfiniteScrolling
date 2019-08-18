c3cb2275bd2afd148c714553861e4a4ce4dcbca9
John Baer <johnbaer3@gmail.com>
2019-08-17 22:55:16 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+// Firebase Config
+const firebaseConfig = {
+    apiKey: "AIzaSyCU7pLC_1XYOVoPGzLj4EVS7lGlVQoNf9c",
+    authDomain: "resolvetest-16fb3.firebaseapp.com",
+    databaseURL: "https://resolvetest-16fb3.firebaseio.com",
+    projectId: "resolvetest-16fb3",
+    storageBucket: "",
+    messagingSenderId: "311353194429",
+    appId: "1:311353194429:web:88986ca74b517bbb"
+  };
+// Exports
+module.exports = firebaseConfig;
